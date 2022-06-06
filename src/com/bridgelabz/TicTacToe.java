1b8f0fe61ca41719173ae9777ebfa125b67b69f5
Ashwini Rathod <rashwini510@gmail.com>
2022-06-06 13:44:04 +0530

    Added:Creating tictactoe board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,9 +1,28 @@
 package com.bridgelabz;
 
 public class TicTacToe {
-     public static void main(String[] args) {
-         System.out.println("Welcome to Tic Tac Toe game");
-     }
+    //Declared a char type array named as gameArray of size 10
+    public static char[] gameArray = new char[10];
+
+    public static void ticTacToeGame() {
+        for (int i = 0; i < gameArray.length; i++) {
+            gameArray[i] = ' ';
+        }
+    }
+
+    //Displaying the game board with 9 empty spaces
+    public static void showBoard() {
+        System.out.println(gameArray[0] + " | " + gameArray[1] + " | " + gameArray[2]);
+        System.out.println(gameArray[3] + " | " + gameArray[4] + " | " + gameArray[5]);
+        System.out.println(gameArray[6] + " | " + gameArray[7] + " | " + gameArray[8]);
+    }
+
+    //Calling ticTacToeGame and showBoard method
+    public static void main(String[] args) {
+        System.out.println("Welcome to tic tac toe game");
+        ticTacToeGame();
+        showBoard();
+    }
 
  }
 
